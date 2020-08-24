993236dc38cd6e4d6616c4e78e7e9da34a25663c
ARKANYOTA <38374187+ARKANYOTA@users.noreply.github.com>
2020-08-24 23:26:50 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
 #Vous pouvez suprimer ce code c'est juste pour le test
 def main():
 	print("Hello World!")
+	print("Langage disponibles :\n\t-Java (J)  \n\tPython (Py)")
+	LangageInitial = input("Quel est le langage du programe :")
+	LangageFinal = input("Quel est le langage du programe :")
+
   
 if __name__ == "__main__:
 	main()
